e6ac1218411371b1d0084dcce6eaf7084c327c21
kilik42 <marvinevins@yahoo.com>
2023-03-05 20:54:02 -0600

    working on the ui

--- index.js
+++ index.js
@@ -18,49 +18,38 @@ let getMovie = () => {
 
             fetch(url).then((resp) => resp.json()).then((data) => {
                 console.log(data);
-                if (data.Response == "False") {
-                    result.innerHTML = `<h3 class = "msg">${data.Error}</h1>`;
-                } else {
-                    result.innerHTML = `<div class = "info">
-                    <img src = "${data.Poster}" class="poster" alt = "">   
-                    <div class = "movie-info">
-                        <h3>${data.Title}</h3>
-
-                            < div class = "rating">
-
-                                    <p>Rating: ${data.imdbRating}</p>
+                if (data.Response == "True") {
+                    result.innerHTML = `
+                        <div class="info">
+                            <img src=${data.Poster} class="poster">
+                            <div>
+                                <h2>${data.Title}</h2>
+                                <div class="rating">
+                                    
+                                    <h4>${data.imdbRating}</h4>
+                                </div>
+                                <div class="details">
+                                    <span>${data.Rated}</span>
+                                    <span>${data.Year}</span>
+                                    <span>${data.Runtime}</span>
+                                </div>
+                                <div class="genre">
+                                    <div>${data.Genre.split(",").join("</div><div>")}</div>
+                                </div>
                             </div>
-                            <div class = "details">
-                                    <p>Released: ${data.Released}</p>
-                                    <p>Runtime: ${data.Runtime}</p>
-                                    <p>Director: ${data.Director}</p>
-                                    
-                                    <p>Actors: ${data.Actors}</p>
-                                    <p>Language: ${data.Language}</p>
-                                    <p>Country: ${data.Country}</p>
-                                    <p>Awards: ${data.Awards}</p>
-                                    <p>BoxOffice: ${data.BoxOffice}</p>
-                                  
-                                    <p>Website: ${data.Website}</p>
-                                    <p>imdbVotes: ${data.imdbVotes}</p>
-                                    
-                                    <p>Type: ${data.Type}</p>
-                                    
-                                    
-                                    <p>${data.Year}</p>
-                                   
-                                    <p>${data.Plot}</p>
-                            </div>
-
-                            <div class = "genre">
-                                    <p>
-                                        Genre: ${data.Genre.split(",").join("</div><div>")}</div>
-                                    </p>
-                           </div>
-                    </div>
-                </div>`;
-                
-     } })
+                        </div>
+                        <h3>Plot:</h3>
+                        <p>${data.Plot}</p>
+                        <h3>Cast:</h3>
+                        <p>${data.Actors}</p>
+                    `;
+                }
+    
+                //if movie doesn't exist in database
+                else {
+                    result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
+                }
+            })
      .catch(() =>{
         result.innerHTML = `<h3 class = "msg">Something went wrong</h1>`;
      })
